1b27d5f6d93ec1d6bc68ebb66ea2f45644e62de3
Salar Khan <salarkhan799354@gmail.com>
2025-07-07 10:03:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCeDWWWlz_ucUU1dm7y6ZQM0nhJLmzIZc8",
   authDomain: "oltech-ai.firebaseapp.com",
   projectId: "oltech-ai",
-  storageBucket: "oltech-ai.appspot.com",
+  storageBucket: "oltech-ai.firebasestorage.app",
   messagingSenderId: "40318640500",
   appId: "1:40318640500:web:c3f34470f27787f9760c21",
   measurementId: "G-NSXTTBXJN3"
